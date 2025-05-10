b50dbac3c4d45482ba2d50381ec05e3d884c71f5
nayem-uddin <mnubpial@gmail.com>
2025-05-10 09:00:55 +0600

    remove strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  // <StrictMode>
+  <App />
+  // </StrictMode>
 );
